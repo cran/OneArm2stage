baa5dbb3c834d444b39fa8f1cffb4d2a9f3e1462
Xiaomeng Yuan <xiaomeng.yuan@stjude.org>
2022-12-01 20:20:02 +0000

    version 1.1.5

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# changes in version 1.1.5
+
+* Enhancements
+  - for Optimal.KJ() and Optimal.rKJ(), now users can choose to specify a minimum value for the early stopping probability under H0 using the argument prStop. Examples about how to implement this new feature are provided in the vignettes and help documentations. 
--- inst/doc/OneArm2stage.R
+++ inst/doc/OneArm2stage.R
@@ -139,7 +139,7 @@ Sim_rKJ(dist="GM", shape=0.5, S0=0.3, S1=0.3^(0.65), x0=1, x=1, rate=10, t1=3.70
 #  ## First, we take a screenshot of the plot and save it as a .png file. In this example it is named as "wang2018.png".
 #  
 #  ## Next, create a path directing to the .png file.
-#  ## e.g. path <-".../wang2018.png"
+#  ## e.g. path <-"./vignettes/wang2018.png"
 #  
 #  ## Last, type the following code in console, and follow the prompts to extract data points by clicking your mouse on the plot.
 #  ## df <- getpoints(path, 0, 24, 0, 100)  # 0,24,0,100 are the ranges of x and y-axes in the image
@@ -155,7 +155,7 @@ trisk <- c(0,2,4,6,8,10,12,14,16,18,20,22,24)
 # number of patients at risk at each risk time point
 nrisk.radio <- c(124,120,115,110,107,104,103,95,46,18,11,8,0) 
 
-# Preprocess the raw coordinates into an proper format for reconstruct IPD
+# Preprocess the raw coordinates into an proper format for reco""nstruct IPD
 pre_radio <- preprocess(dat=df, trisk=trisk, nrisk=nrisk.radio,totalpts=NULL,maxy=100)
 
 #Reconstruct IPD
@@ -177,4 +177,41 @@ modelWB<- FitDat(dat3)
 modelWB$AIC
 
 modelWB$parameter.estimates
+
+## ----eval=F-------------------------------------------------------------------
+#  Optimal.rKJ(dist="WB", shape=1, S0=0.62, x0=2, hr=0.467, x=2, rate=5,
+#                    alpha=0.05, beta=0.2, prStop=0.65)
+#  
+#  # $Two_stage
+#  #   n1    c1  n      c     t1 MTSL      ES     PS
+#  # 1 30 0.386 46 1.6116 5.8247 11.2 35.0258 0.6503
+
+## ----eval=F-------------------------------------------------------------------
+#  # early stopping probability under H0
+#  P <- t(seq(0.6, 0.85, l=6))
+#  
+#  # a design without pre-defined stopping probability
+#  o1 <- Optimal.rKJ(dist="WB", shape=1, S0=0.62, x0=2, hr=0.467, x=2, rate=5,
+#                    alpha=0.05, beta=0.2)
+#  
+#  # designs with varying pre-defined stopping probabilities
+#  res1 <- t(apply(P, 2, Optimal.rKJ,
+#                  dist="WB", shape=1, S0=0.62, x0=2, hr=0.467, x=2, rate=5, alpha=0.05, beta=0.2))
+#  
+#  # compare results
+#  r1 <- rbind(o1$Two_stage,
+#              res1[[1]]$Two_stage, res1[[2]]$Two_stage,
+#              res1[[3]]$Two_stage, res1[[4]]$Two_stage,
+#              res1[[5]]$Two_stage, res1[[6]]$Two_stage)
+#  
+#  # first row shows the design without pre-defined stopping probability (prStop=NULL)
+#  # > r1
+#  #   n1     c1  n      c     t1 MTSL      ES     PS
+#  # 1 26 0.1349 46 1.6171 5.0946 11.2 34.6350 0.5537
+#  # 2 28 0.2544 46 1.6147 5.4413 11.2 34.7161 0.6004
+#  # 3 30 0.3860 46 1.6116 5.8247 11.2 35.0258 0.6503
+#  # 4 31 0.5247 47 1.6013 6.1331 11.4 35.5642 0.7001
+#  # 5 33 0.6760 47 1.5952 6.5867 11.4 36.4433 0.7505
+#  # 6 36 0.8436 46 1.5927 7.1199 11.2 37.6740 0.8005
+#  # 7 39 1.0382 46 1.5768 7.6639 11.2 39.4686 0.8504
 
